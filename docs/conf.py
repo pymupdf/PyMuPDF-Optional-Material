c09788526352945204fe699cc7ae0271d560e5c6
Jorj X. McKie <jorj.x.mckie@outlook.de>
2019-10-04 03:22:11 -0400

    html theme change

--- docs/conf.py
+++ docs/conf.py
@@ -94,9 +94,9 @@ pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-# html_theme = "agogo"
+html_theme = "agogo"
 # html_theme = "sphinxdoc"
-html_theme = "python_docs_theme"
+#html_theme = "python_docs_theme"
 # html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
